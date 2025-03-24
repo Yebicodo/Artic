c6d9fb13d54b3a302c67d76ae5533863051fc56e
Gence <vivierayan@gmail.com>
2025-03-24 20:14:45 +0400

    sauvegarde

--- index.php
+++ index.php
@@ -24,6 +24,28 @@
     <img src="image3.png" alt="Portrait Abstrait 3">
   </section>
 
+  <section class="section">
+    <h1>Le Salon des Beaux Arts 2024</h1>
+    <p>
+Dossier de presse – Le Salon des Beaux Arts 2024
+Les 160 ans du Salon des Beaux Arts : le Salon d’hiver !
+Pendant deux semaines, du 4 au 15 décembre 2024 au Réfectoire des Cordeliers Paris 6e, le Salon des Beaux Arts vous invite à découvrir les œuvres contemporaines d’artistes venu·e·s du monde entier dans les sections Naturaliste, Papier-Papiers, Peinture, Photographie, Sculpture et Textile.
+
+Créé en 1864, par des artistes pour des artistes, la vocation du Salon est de soutenir la création, de mettre en lumière des talents et permettre l’envol d’artistes émergent·e·s. Le Salon est ainsi un soutien unique à la création et un outil indispensable permettant aux talents d’affirmer leur indépendance et leur liberté.
+
+Premier Salon à avoir accueilli les artistes étrangers (Brancusi, Foujita ou Zao Wou-Ki) et les artistes femmes (Suzanne Valadon, Pān Yùliáng, Camille Claudel), le Salon des Beaux Arts s’engage également, depuis l’origine, sur des partenariats originaux, qui font sa force et sa singularité. 
+Cette année encore, de nombreux prix partenaires viendront doter des artistes exposé·e·s.</p>
+<img src="img index/pexels-unalaurencic-20967.jpg" alt="img musée">
+
+<h1>Avis aux amateurs de street-art : le Cabaret du Chat Noir dévoile 3 000 m2 de créations et réunit 350 artistes dans son Expo 150 ! </h1>
+ <p>Au cœur d'un ancien central téléphonique, les graffitis et les dessins se mêlent aux peintures à la bombe et aux œuvres chimériques. Véritable scène ouverte, le Chat Noir a donné carte blanche et une liberté complète aux artistes afin de transcender ce lieu en un espace d'art, de partage et de contemporanéité. Digne héritier du cabaret du XIXe siècle, le Chat Noir s'inscrit dans cette célébration des artistes indépendants et souvent mis de côté dans les institutions. Le street-art peut s'épanouir ici, occuper son espace et s'en emparer pour le sublimer. La myriade impressionnante de créateurs réunis pour l'occasion promet un spectacle grisant : une transdisciplinarité comme mot d'ordre et une cascade de styles, de couleurs et de voix différentes comme tableau. Ici, il faut regarder partout, prêter attention aux murs, aux plafonds et aux parterres, peuplés d'anamorphoses vertigineuses. Pour consacrer la dynamique de l'exposition entièrement gratuite, différentes activités prendront place et participeront à enrichir l'esprit du lieu, et atelier de poésie, DJ-set et performances rythmeront les soirées de cet espace incroyable.</p>
+ <img src="img index\pexels-mikhail-nilov-6972775.jpg" alt="img ">
+
+ <h1>A la gloire de l'empereur</h1>
+ <p>Seconde chapelle du château (1672-1682), puis grande salle des Gardes « commune au Roi et à la Reine » (1682-1789), ce grand espace qui précède les appartements des souverains devint en 1833 une salle consacrée à la gloire de Napoléon Bonaparte, Premier Consul, puis empereur des Français. On prévoyait d’y installer les plus grands tableaux de son règne, qui ne pouvaient trouver place ailleurs dans le château : les deux immenses toiles commandées au peintre Jacques-Louis David pour commémorer les cérémonies de l’avènement de Napoléon comme Empereur des Français : le SacreDécouvrir le tableau sur le site Collections..., Découvrir le tableau sur le site Collections... le 2 décembre 1804 (1805-1808), et la Distribution des aigles à l’armée, le 5 décembre 1804 (1808-1810). Le décor qui sert d’écrin à ces œuvres est dû aux talents conjugués de l’architecte du palais, Frédéric Nepveu, du sculpteur Jean-Baptiste PlantarJean-Baptiste Louis Plantar (1790-1879) est un ornemaniste français. Il est le dernier artiste à porter le titre de sculpteur des bâtiments du Roi. (trophées sur les boiseries) et de l’atelier du peintre Jean AlauxJean Alaux (1786-1854) est un peintre français, condisciple d'Horace Vernet chez Pierre-Narcisse Guérin. Il fut, entre autres, chargé des peintures de la salle des généraux du château de Versailles. Il fut directeur de l'académie de France à Rome et membre de l'Académie des beaux-arts. (voussure du plafond).</p>
+<img src="img index\serment_de_larmee_fait_a_lempereur_apres_la_distribution_des_aigles_5_decembre_1804.jpg" alt="img 2">
+
+</section>
   <footer>
     &copy; 2025 Artic. Tous droits réservés.
   </footer>
